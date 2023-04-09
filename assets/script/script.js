1790b6dc84d11075bab4361a78133b1128a2296e
Simon Paske <45258462+SimonPaske@users.noreply.github.com>
2023-04-09 14:37:21 +0000

    Add empty functions to have clear further steps.

---
+++ assets/script/script.js
@@ -0,0 +1,30 @@
+
+
+function generatePuzzle() {
+
+}
+
+function fillArray() {
+
+}
+
+function shuffleArray() {
+
+}
+
+function moveCounter() {
+
+}
+
+function squareMoves() {
+
+}
+
+function checkWin() {
+
+}
+
+function replacePhotoArray() {
+
+}
+
