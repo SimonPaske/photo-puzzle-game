66ec419c7cc1166bc8318b2d37a78caf678eede0
Simon Paske <45258462+SimonPaske@users.noreply.github.com>
2023-04-09 14:39:36 +0000

    Add for loop to fillArray function

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,22 @@
 
+/**
+ * This function fills the game board with squares and adds photos to them.
+ */
+function fillArray() {
 
-function generatePuzzle() {
+    // Clear out any existing images in the game-container
+    document.getElementById("game-container").innerHTML = "";
 
+    // Create 9 squares and add them to the game-container
+    for (let i = 0; i < 9; i++) {
+        let square = document.createElement("img");
+        square.id = `square${i}`;
+
+        square.src = `assets/images/${chosenFolder}/0${photoArray[i]}.webp`;
+        document.getElementById("game-container").appendChild(square);
+        photoArray[i] = i;
+        
+    }
 }
 
 function fillArray() {
