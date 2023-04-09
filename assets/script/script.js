5e3e07621a975791e7079680ef5fa76f0a0402d0
Simon Paske <45258462+SimonPaske@users.noreply.github.com>
2023-04-09 14:41:04 +0000

    Add for loop to shuffleArray function

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,12 +19,23 @@ function fillArray() {
     }
 }
 
-function fillArray() {
-
-}
-
+/**
+ * This function randomizes the order of the photos in the game board.
+ * It is called when the page loads.
+ */
 function shuffleArray() {
 
+    for (let i = 0; i < 9; i++) {
+        photoArray.sort(() => Math.random() - 0.5);
+        let randomIndex = Math.floor(Math.random() * 9);
+        let temp = photoArray[i];
+        photoArray[i] = photoArray[randomIndex];
+        photoArray[randomIndex] = temp;
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(`square${i}`).src = `assets/images/${chosenFolder}/0${photoArray[i]}.webp`;
+    }
 }
 
 function moveCounter() {
